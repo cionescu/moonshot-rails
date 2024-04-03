26b695c52328cc7a0a5978979aca7c6527e86a53
Catalin Ionescu <catalin.ionescu282@gmail.com>
2024-04-03 11:13:42 +0300

    Spec unsubscribe

--- spec/dummy/spec/models/user_spec.rb
+++ spec/dummy/spec/models/user_spec.rb
@@ -4,10 +4,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'convertkit' do
+    let(:form_id) { MoonshotRails.configuration.convertkit_form_id }
+    let(:user) { User.create! email: 'test@testy.com' }
+
     before do
       allow(Rails.env).to receive(:production?).and_return(true)
       stub_convertkit_list_subscribers
       stub_convertkit_add_subscriber_to_form
+      stub_convertkit_unsubscribe
     end
 
     around do |example|
@@ -17,10 +21,19 @@ RSpec.describe User, type: :model do
     end
 
     it 'syncs to convertkit on create' do
-      User.create! email: 'test@testy.com'
+      user
 
       expect(WebMock).to(
-        have_requested(:post, %r{api.convertkit.com/v3/forms/#{MoonshotRails.configuration.convertkit_form_id}/subscribe})
+        have_requested(:post, %r{api.convertkit.com/v3/forms/#{form_id}/subscribe})
+          .with body: hash_including(email: 'test@testy.com')
+      )
+    end
+
+    it 'unsuscribes from convertkit on destroy' do
+      user.destroy!
+
+      expect(WebMock).to(
+        have_requested(:put, %r{api.convertkit.com/v3/unsubscribe})
           .with body: hash_including(email: 'test@testy.com')
       )
     end
